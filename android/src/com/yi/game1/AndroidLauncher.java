cf18a740662e36408b06124a9efc642cc4c39eee
yimb <missionbit@missionbits-MacBook-Pro-2.local>
2017-06-01 17:59:22 -0700

    new libgdx project to github

---
+++ android/src/com/yi/game1/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.yi.game1;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.yi.game1.YiGdxGame1;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new YiGdxGame1(), config);
+	}
+}
---
+++ desktop/src/com/yi/game1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.yi.game1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.yi.game1.YiGdxGame1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new YiGdxGame1(), config);
+	}
+}
